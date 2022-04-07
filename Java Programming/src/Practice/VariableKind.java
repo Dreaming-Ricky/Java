ad01c6f8689150a935665ed8c0c8fe2c4e072034
나준희 <98261698+Dreaming-Ricky@users.noreply.github.com>
2022-04-07 16:03:05 +0900

    test0407

--- Java Programming/src/Practice/VariableKind.java
+++ Java Programming/src/Practice/VariableKind.java
@@ -10,16 +10,16 @@ public class VariableKind {
 		int localVar = 0; // 로컬 변수
 		
 		VariableKind vk1 = new VariableKind();
-		System.out.println("vk1.memVar = " +vk1.memVar);
+		System.out.println("vk1.memVar = "+vk1.memVar);
 		System.out.println("vk.staticVar ="+vk1.staticVar);
 		System.out.println("VariableKind.staticVar ="+VariableKind.staticVar);
 		System.out.println("staticVar ="+staticVar);
 		
 		
 		VariableKind vk2 = new VariableKind();
-		staticVar = 20;
-		System.out.println("vk2.staticVar ="+vk2.staticVar); // System.out.println("localVar ="+localVar);
+		vk1.staticVar = 20;
+		System.out.println("vk2.staticVar ="+vk2.staticVar); // System.out.println("localVar ="+localVar); //왜 20이 출력되는지 이해하고 설명하기
 		System.out.println("localVar ="+localVar);
 	}
 	
-}
+}// variablekind2 만들어서 static 접근시에는 오류가 뜨는지 알아보기
